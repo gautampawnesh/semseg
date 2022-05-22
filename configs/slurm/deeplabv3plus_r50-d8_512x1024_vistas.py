c4bef4a4a4f1213cebc8187eab34794f20386747
pawnesh kumar <pawnesh.ranvijay.gautam@gmail.com>
2022-05-23 00:33:20 +0200

    added config for vistas

---
+++ configs/slurm/deeplabv3plus_r50-d8_512x1024_vistas.py
@@ -0,0 +1,75 @@
+experiment = dict(
+    name="Mapillary vistas Training",
+    description="MV classes mapped to universal classes with flat model ",
+)
+# directory to save logs and models
+work_dir = "/netscratch/gautam/semseg/results/vistas_train_deeplabv3plus_65c/"
+# random seed
+seed = 1
+# launcher
+launcher = "slurm"
+dist_params = dict(backend="nccl")
+# checkpoint file to load weights from
+load_from = None
+# checkpoint file to resume from
+resume_from = None
+
+_base_ = [
+    '../_base_/models/deeplabv3plus_r50-d8.py',
+    '../_base_/datasets/vistas_512_1024.py',
+]
+ignore_index = 255
+data = dict(samples_per_gpu=8,
+            workers_per_gpu=2,
+            test=dict(ignore_index=ignore_index),
+            train=dict(ignore_index=ignore_index),
+            val=dict(ignore_index=ignore_index))
+
+model = dict(
+    decode_head=dict(ignore_index=ignore_index, num_classes=66),
+    auxiliary_head=dict(ignore_index=ignore_index, num_classes=66),
+)
+
+workflow = [("train", 1), ("val", 1)]
+
+# optimizer
+optimizer = dict(
+    type='SGD',
+    lr=0.01,
+    momentum=0.9,
+    weight_decay=0.0005)
+optimizer_config = dict()
+# learning policy
+lr_config = dict(
+    policy='poly',
+    power=0.9,
+    min_lr=1e-4,
+    by_epoch=True)
+# runtime settings
+runner = dict(
+    type='EpochBasedRunner',
+    max_epochs=100)
+# checkpoints settings
+checkpoint_config = dict(
+    by_epoch=True,
+    interval=5,
+    max_keep_ckpts=5,
+    create_symlink=False
+)
+evaluation = dict(
+    interval=1000,
+    metric='mIoU',
+    gpu_collect=True,
+    pre_eval=True,
+    save_best="mIoU")
+
+
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ])
+
+log_level = 'INFO'
+cudnn_benchmark = True
