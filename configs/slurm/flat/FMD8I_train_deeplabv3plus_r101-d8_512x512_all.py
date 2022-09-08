791b3280df9f3b8bf9c66ded53eda0a86650b4b3
pawnesh kumar <pawnesh.ranvijay.gautam@gmail.com>
2022-09-08 11:01:53 +0200

    updated config for exper

---
+++ configs/slurm/flat/FMD8I_train_deeplabv3plus_r101-d8_512x512_all.py
@@ -0,0 +1,59 @@
+experiment = dict(
+    name="FMD 8 Flat Deeplabv3+  All 9 Training os8 1L:1L:1L:1L:1L:1L:1K:1K:1K",
+    description=" All 9 dataset classes mapped to universal classes with flat deeplabv3+ model  \ "
+)
+# directory to save logs and models
+work_dir = "/netscratch/gautam/semseg/final_weights/FMD8/"
+# random seed
+seed = 1
+# checkpoint file to load weights from
+load_from = None
+# checkpoint file to resume from
+resume_from = None
+
+_base_ = [
+    '../../_base_/default_runtime.py',
+    '../../_base_/models/flat/FMD8_flat_train_deeplabv3plus_r101-d8.py',
+    '../../_base_/datasets/flat/FMD8_all_512_512.py',
+]
+ignore_index = 0
+
+data = dict(samples_per_gpu=8,
+            workers_per_gpu=6)
+
+model = dict(
+    decode_head=dict(ignore_index=ignore_index, num_classes=191)
+)
+
+# optimizer
+optimizer = dict(
+    type='SGD',
+    lr=0.007,
+    momentum=0.9,
+    weight_decay=0.0005
+)
+optimizer_config = dict(type="Fp16OptimizerHook", loss_scale=512.0)
+
+# learning policy
+lr_config = dict(
+    policy='poly',
+    power=0.9,
+    min_lr=1e-5,
+    by_epoch=True)
+
+# runtime settings
+runner = dict(
+    type='EpochBasedRunner',
+    max_epochs=10)
+
+# checkpoints settings
+checkpoint_config = dict(
+    by_epoch=True,
+    interval=1,
+    max_keep_ckpts=15,
+    create_symlink=False
+)
+
+evaluation = dict(_delete_=True, interval=1, metric="mIoU", gpu_collect=True, pre_eval=True, save_best="mIoU")
+
+log_level = 'INFO'
---
+++ configs/slurm/hiera/HA4_deeplabv3plus_r101-d8_512x512_hierarchical_all.py
@@ -0,0 +1,68 @@
+experiment = dict(
+    name="HA3 Hierarchical All 9 Training 1L:1L:1L:1L:1L:1L:1K:1K:1K",
+    description=" All 9 dataset classes mapped to universal classes with flat model   "
+)
+# directory to save logs and models
+work_dir = "/netscratch/gautam/semseg/final_weights/HA3_1/"
+# random seed
+seed = 1
+# checkpoint file to load weights from
+#load_from = "/netscratch/gautam/semseg/final_weights/HA3_1/training/20220804_044105/epoch_10.pth"
+load_from = "/netscratch/gautam/semseg/final_weights/HA3_1/training/20220814_010056/epoch_6.pth"
+# checkpoint file to resume from
+resume_from = None
+
+# Todo: update backbone pretrained checkpoint in model file
+
+_base_ = [
+    '../../_base_/default_runtime.py',
+    '../../_base_/models/hierarchical/HA3_1_hierarchicalv1_deeplabv3plus_r101-d8_w_ohem_w_loss_weight.py',
+    '../../_base_/datasets/hiera/HA3_hierarchical_all_512_512.py',
+]
+ignore_index = 0
+
+## Todo: Check samples per gpu ? keep max
+
+data = dict(samples_per_gpu=2,
+            workers_per_gpu=6)
+
+
+# optimizer
+optimizer = dict(
+    type='SGD',
+    #lr=0.007,
+    #lr=0.0007,
+    lr=0.0004,
+    momentum=0.9,
+    weight_decay=0.0005)
+
+## Todo: As we removed Aux heads, Is memory sufficient for FP32 ?? No
+#optimizer_config = dict()
+
+optimizer_config = dict(type="Fp16OptimizerHook", loss_scale=512.0)
+
+# learning policy
+lr_config = dict(
+    policy='poly',
+    power=0.9,
+    #min_lr=1e-5,
+    #min_lr=1e-6,
+    min_lr=1e-5,
+    by_epoch=True)
+
+# runtime settings
+runner = dict(
+    type='EpochBasedRunner',
+    max_epochs=10)
+
+# checkpoints settings
+checkpoint_config = dict(
+    by_epoch=True,
+    interval=1,
+    max_keep_ckpts=15,
+    create_symlink=False
+)
+
+evaluation = dict(_delete_=True, interval=1, metric="mIoU", gpu_collect=True, pre_eval=True, save_best="mIoU")
+
+log_level = 'INFO'
