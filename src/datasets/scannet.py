5974488bb550c466ffc38f5a74e37f5e6767c996
pawnesh kumar <pawnesh.ranvijay.gautam@gmail.com>
2022-09-24 20:08:42 +0200

    scannet dataset supports benchmarking

--- src/datasets/scannet.py
+++ src/datasets/scannet.py
@@ -78,6 +78,9 @@ class UniversalScannetDataset(BaseDataset):
         if self.img_meta_data:
             data_df = pd.read_csv(self.img_meta_data)
             data_df = data_df.sort_values("image")
+        if self.benchmark:
+            images = list(Path(self.img_dir).glob(f"**/*{self.img_suffix}"))
+            data_df = pd.DataFrame.from_dict({"image": images})
         elif self.split:
             images, labels = [], []
             with open(self.split, "r") as fp:
