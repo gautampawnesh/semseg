d484555d2a95a1be5fb1520019a4b1d9dcd6047b
pawnesh kumar <pawnesh.ranvijay.gautam@gmail.com>
2022-10-08 20:02:24 +0200

    configs for inference

---
+++ configs/inference/hiera_improve_HA4_inference.py
@@ -0,0 +1,26 @@
+experiment = dict(
+    name="HA4 Hierarchical Inference",
+    description=" All 9 dataset classes mapped to universal classes with flat model   "
+)
+
+#checkpoint="/netscratch/gautam/semseg/final_weights/HA4_test/training/20220923_015504/epoch_10.pth"
+# directory to save logs and models
+work_dir = "/netscratch/gautam/semseg/final_weights/HA4_test/"
+# random seed
+seed = 1
+
+load_from = None
+
+resume_from = None
+
+_base_ = [
+    '../_base_/default_runtime.py',
+    '../_base_/models/hierarchical/HA4_hierarchicalv1_deeplabv3plus_r101-d8_w_ohem_w_loss_weight.py',
+    '../_base_/datasets/hiera/HA4_hierarchical_all_512_512.py',
+]
+ignore_index = 0
+
+data = dict(samples_per_gpu=2,
+            workers_per_gpu=6)
+
+log_level = 'INFO'
